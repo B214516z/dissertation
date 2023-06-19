c39d4f74bca36bbf2b500d8f55f285cfd50f806b
B214516z <114988809+B214516z@users.noreply.github.com>
2023-06-19 15:15:16 +0100

    Update load_huggingface.py

--- load_huggingface.py
+++ load_huggingface.py
@@ -1,7 +1,3 @@
 from datasets import load_dataset
 #data_link = "https://huggingface.co/datasets/google/fleurs/blob/main/data/cy_gb/dev.tsv"
 fleurs_asr = load_dataset("google/fleurs", "cy_gb", cache_dir="./huggingface/")
-print(fleurs_asr)
-
-audio_input = fleurs_asr["train"][0]["audio"]
-transcription = fleurs_asr["train"][0]["transcription"]
