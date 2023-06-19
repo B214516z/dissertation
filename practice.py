ea751eb0c6e229a74be338f9317e577e3d04e58d
B214516z <114988809+B214516z@users.noreply.github.com>
2023-06-19 15:52:34 +0100

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,7 @@
+from datasets import load_dataset
+#data_link = "https://huggingface.co/datasets/google/fleurs/blob/main/data/cy_gb/dev.tsv"
+fleurs_asr = load_dataset("google/fleurs", "cy_gb", cache_dir="./huggingface/")
+
+print(fleurs_asr)
+
+audio_input = fleurs_asr["train"][0]["audio"]
