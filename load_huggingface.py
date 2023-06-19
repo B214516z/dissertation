e95bfb84a24ec61856e38fc2e7f8c00e4801c78f
B214516z <114988809+B214516z@users.noreply.github.com>
2023-06-19 15:25:18 +0100

    Update load_huggingface.py

--- load_huggingface.py
+++ load_huggingface.py
@@ -1,3 +1,5 @@
 from datasets import load_dataset
 #data_link = "https://huggingface.co/datasets/google/fleurs/blob/main/data/cy_gb/dev.tsv"
 fleurs_asr = load_dataset("google/fleurs", "cy_gb", cache_dir="./huggingface/")
+
+print("\n👉 First item 'fleurs_asr[0]':")
